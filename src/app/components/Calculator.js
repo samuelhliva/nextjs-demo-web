1918f50fabe46a0605de0181306bea349d76375a
Samuel Hliva <samuel.hliva@student.tuke.sk>
2025-06-23 21:49:23 +0200

    ft: create a calc form component

--- src/app/components/Calculator.js
+++ src/app/components/Calculator.js
@@ -1,19 +1,7 @@
-'use client'
-
-import { useState } from "react"
 import styles from "./calculator.module.css"
+import CalculatorForm from "./CalculatorForm"
 
 export default function Calculator() {
-    const [amount, setAmount] = useState(0);
-    const [timerange, setTimerange] = useState(6);
-
-    const handleAmountChange = (e) => {
-        setAmount(e.target.value);
-    }
-
-    const handleTimerangeChange = (e) => {
-        setTimerange(e.target.value);
-    }
 
     return (
         <div className={styles.calculator}>
@@ -21,37 +9,7 @@ export default function Calculator() {
                 <h2>Setup your loan</h2>
             </div>
 
-            <form action={undefined}
-                className={styles.setup_form}
-            >
-                <div className={styles.form_input}>
-                    <label htmlFor="ammountInput">Amount</label>
-                    <input name="amount"
-                        id="ammountInput" 
-                        type="range" 
-                        min={1} 
-                        max={100000} 
-                        step={100}
-                        onChange={handleAmountChange}
-                    />
-                    <span className={styles.info_span}>The ammount is {amount}</span>
-                </div>
-                
-                <div className={styles.form_input}>
-                    <label htmlFor="timerangeInput">Time Range</label>
-                    <input name="timerange" 
-                        id="timerangeInput"
-                        type="range" 
-                        min={6} 
-                        max={60} 
-                        step={6}
-                        onChange={handleTimerangeChange}
-                    />
-                    <span className={styles.info_span}>The time range is {timerange}</span>
-                </div>
-
-                <input type="submit" value={"Apply"} className={styles.submit_btn} />
-            </form>
+            <CalculatorForm />
         </div>
     )
 }
